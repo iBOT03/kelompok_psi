a618b284eb9cb08ac665688edddd18c2535b01f3
Moh. Saidul Musthofa <sayyidmusthofa89@gmail.com>
2020-11-03 11:14:47 +0700

    Revert "base_url"
    This reverts commit c00c821247002ac4bc3a1635bb46f13413a917d1.

--- cafe_rooster/application/config/config.php
+++ cafe_rooster/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kelompok_psi/cafe_rooster/';
+$config['base_url'] = 'http://localhost/group_psi/cafe_rooster/';
 
 /*
 |--------------------------------------------------------------------------
