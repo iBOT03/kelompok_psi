87f0abcc2836ea224375c512c2de143ca4e314c6
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-12-28 19:25:23 +0700

    Update lap_transaksi.php

--- cafe_rooster/application/views/admin/transaksi/lap_transaksi.php
+++ cafe_rooster/application/views/admin/transaksi/lap_transaksi.php
@@ -24,7 +24,7 @@
                   <div class="col-12">
                       <div class="card">
                           <div class="card-header">
-                              <a href="<?= base_url("admin/pengguna/tambah") ?>" class="btn btn-sm btn-info btn-icon-split shadow-sm">
+                              <a href="" class="btn btn-sm btn-info btn-icon-split shadow-sm" onclick="printDiv('print-area')">
                                   <span class="icon text-white">
                                       <i class="fas fa-download"></i>
                                       <i class="text">Cetak Laporan</i>
@@ -84,9 +84,78 @@
           <!-- /.container-fluid -->
       </section>
       <!-- /.content -->
+      <script>
+          $(document).ready(function() {
+              $('.input-daterange').datepicker({
+                  todayBtn: 'linked',
+                  format: "yyyy-mm-dd",
+                  autoclose: true
+              });
+              $('#example1').DataTable({
+                  dom: 'Blfrtip',
+                  buttons: [{
+                          extend: 'csvHtml5',
+                          exportOptions: {
+                              columns: [0, 1, 2, 3, 4, 5, ],
+                          },
+                      },
+                      {
+                          extend: 'excelHtml5',
+                          title: 'LAPORAN PENJUALAN',
+                          exportOptions: {
+                              columns: [0, 1, 2, 3, 4, 5],
+                          },
+                      },
+                      {
+                          extend: 'copyHtml5',
+                          title: 'LAPORAN PENJUALAN',
+                          exportOptions: {
+                              columns: [0, 1, 2, 3, 4, 5],
+                          },
+                      },
+                      {
+                          extend: 'pdfHtml5',
+                          oriented: 'portrait',
+                          pageSize: 'legal',
+                          title: 'LAPORAN PENJUALAN',
+                          download: 'open',
+                          exportOptions: {
+                              columns: [0, 1, 2, 3, 4, 5],
+                          },
+                          customize: function(doc) {
+                              doc.content[1].table.widths = Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+                              doc.styles.tableBodyEven.alignment = 'center';
+                              doc.styles.tableBodyOdd.alignment = 'center';
+                          },
+                      },
+                      {
+                          extend: 'print',
+                          oriented: 'portrait',
+                          pageSize: 'A4',
+                          title: 'LAPORAN PENJUALAN',
+                          exportOptions: {
+                              columns: [0, 1, 2, 3, 4, 5],
+                          },
+                      },
+                  ],
+              });
+          });
+      </script>
   </div>
   <!-- /.content-wrapper -->
 
   </div>
   <!-- ./wrapper -->
   </body>
+  <!-- print nota -->
+  <!-- <script type="text/javascript">
+  function printDiv(divName) {
+      let printContents = document.getElementById(divName).innerHTML;
+      let originalContents = document.body.innerHTML;
+      document.body.innerHTML = printContents;
+      window.print();
+      document.body.innerHTML = originalContents;
+      location.reload(true);
+      setTimeout(function() {}, 1000);
+  }
+</script> -->
