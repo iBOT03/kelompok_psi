209f5eee3fb396dbc98546e7e6647d2e17fd5dd9
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-01-09 23:03:52 +0700

    .

--- cafe_rooster/application/views/user/Booking/booking.php
+++ cafe_rooster/application/views/user/Booking/booking.php
@@ -31,19 +31,19 @@
                                 <th scope="row"><?= $i ?></th>
                                 <td><?= $data['tgl_acara'] ?></td>
                                 <td><?= $data['jumlah_meja'] ?></td>
-                                <td>Rp. <?= number_format ($data['total_booking']) ?></td>
-                                <td>Rp. <?= number_format ($data['dp_booking']) ?></td>
+                                <td>Rp. <?= number_format($data['total_booking']) ?></td>
+                                <td>Rp. <?= number_format($data['dp_booking']) ?></td>
                                 <td> <?php if ($data['status_transaksi'] == 1) {
                                             echo '<p class="badge bg-warning" style="width: 11rem;">Menunggu verifikasi pesanan</p>';
-                                        }else if($data['status_transaksi'] == 3){
+                                        } else if ($data['status_transaksi'] == 3) {
                                             echo '<p class="badge bg-primary" style="width: 11rem;">Menunggu verifikasi bukti</p>';
-                                        }else if($data['status_transaksi'] == 4){
+                                        } else if ($data['status_transaksi'] == 4) {
                                             echo '<p class="badge bg-success" style="width: 11rem;">Booking selesai</p>'; ?>
-                                        <?php } else { ?>
-                                            <a class="btn btn-primary js-scroll-trigger" href="#" data-toggle="modal" role="button" data-target="#bayarModal<?= $data['id_booking']; ?>" >
+                                    <?php } else { ?>
+                                        <a class="btn btn-primary js-scroll-trigger" href="#" data-toggle="modal" role="button" data-target="#bayarModal<?= $data['id_booking']; ?>">
                                             <i class="mr-2 text-gray-400"></i>
                                             Lakukan pembayaran
-                                        </a><?php ;
+                                        </a><?php
                                         } ?>
                                 </td>
                             </tr>
@@ -80,53 +80,53 @@
 </section>
 
 
-<?php $no = 0;
-foreach($booking as $data) : $no++; ?>
-<div class="modal fade" id="bayarModal<?= $data['id_booking'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Upload bukti pembayaran</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
+<?php
+foreach ($booking as $data) :  ?>
+    <div class="modal fade" id="bayarModal<?= $data['id_booking']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Upload bukti pembayaran</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <form action="<?= base_url('Booking/UploadBukti') ?>" method="post" enctype="multipart/form-data">
+                    <div class="row g-3">
+                        <input type="hidden" name="idBooking" id="idBooking" value="<?= $data['id_booking']; ?>">
+                        <div class="col-sm-5">
+                            <label for="tglAcara">Tanggal acara</label>
+                            <p id="tglAcara"><?= $data['tgl_acara']; ?></p>
+                        </div>
+                        <div class="col-sm">
+                            <label for="tglAcara">DP</label>
+                            <p>Rp. <?= number_format($data['dp_booking']); ?></p>
+                        </div>
+                        <div class="col-sm">
+                            <label for="tglAcara">Total tagihan</label>
+                            <p>Rp. <?= number_format($data['total_booking']); ?></p>
+                        </div>
+                    </div>
+                    <div class="alert alert-secondary text-center" role="alert">
+                        Silahkan bayar (DP/Lunas) untuk booking yang anda pesan.<br>
+                        Pastikan anda mentransfer pembayaran anda ke rekening BCA berikut, lalu upload bukti pembayaran dibawah ini. <br>
+                        <center><b>2010847583, Atas nama Hendry Dwi Nurmansyah Idris</b></center>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="file" name="buktitf" class="form-control" id="inputBukti" required>
+                        <label class="input-group-text" for="inputBukti">Upload</label>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-info" type="button" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-success">Kirim</button>
+                    </div>
+
+                </form>
+
+                
+                
+                <?php endforeach;?>
+                
             </div>
-            <form action="<?= base_url('Booking/UploadBukti') ?>" method="post" enctype="multipart/form-data">
-            <div class="row g-3">
-                <input type="hidden" name="idBooking" id="idBooking" value="<?= $data['id_booking'];?>">
-                <div class="col-sm-5">
-                    <label for="tglAcara">Tanggal acara</label>
-                    <p id="tglAcara"><?= $data['tgl_acara'];?></p>
-                </div>
-                <div class="col-sm">
-                    <label for="tglAcara">DP</label>
-                    <p>Rp. <?= number_format ($data['dp_booking']);?></p>
-                </div>
-                <div class="col-sm">
-                    <label for="tglAcara">Total tagihan</label>
-                    <p>Rp. <?= number_format ($data['total_booking']);?></p>
-                </div>
-                </div>
-                <div class="alert alert-secondary text-center" role="alert">
-                    Silahkan bayar (DP/Lunas) untuk booking yang anda pesan.<br>
-                    Pastikan anda mentransfer pembayaran anda ke rekening BCA berikut, lalu upload bukti pembayaran dibawah ini. <br>
-                    <center><b>2010847583, Atas nama Hendry Dwi Nurmansyah Idris</b></center>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="file" name="buktitf" class="form-control" id="inputBukti" required>
-                    <label class="input-group-text" for="inputBukti">Upload</label>
-                </div>
-                <div class="modal-footer">
-                <button class="btn btn-info" type="button" data-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-success">Kirim</button>
-            </div>
-
-            </form>
-
-            <?php endforeach; ?>
-
-            
-            
         </div>
     </div>
-</div>
